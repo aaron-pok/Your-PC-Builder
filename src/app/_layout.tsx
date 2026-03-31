a95d7442f90cd6dbcecce8bb66239a9a3e490cac
aaron-pok <frostdada147@gmail.com>
2026-03-31 12:14:47 +0545

    Move _layout.tsx to src/app/

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+// This is the updated layout file for the application
+
+// Your layout logic here
+
+export default function Layout() {
+    return (
+        <div>Your layout components here</div>
+    );
+}
